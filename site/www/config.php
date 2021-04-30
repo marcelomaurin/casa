c64363d99f91e64008371e8e85073519885f5cf3
marcelomaurin <marcelomaurinmartins@gmail.com>
2021-04-30 07:56:32 -0300

    Mudanças no projeto

--- site/www/config.php
+++ site/www/config.php
@@ -1,12 +1,12 @@
 ﻿<?php
     //Inclua aqui as configurações do seu banco de dados
-	$dbhost        = '192.168.0.213';
+	$dbhost        = 'localhost';
 	$database      = 'casadb';
 	$dbuser        = 'root';
-    $dbpassword    = '226468';
-	$urlsrv        = 'maurinsoft.com.br';
+    $dbpassword    = 'password';
+	$urlsrv        = 'www.your.site';
 	$URLLABEL      = 'Casa Inteligente';	
-	$emailreply    = 'marcelomaurinmartins@gmail.com';
+	$emailreply    = 'your@email';
 	$titlesite     = 'Projeto Casa Inteligente';
 	$erro_redirect = 'construcao.php';
 	$erro_access = 'notaccess.php';
