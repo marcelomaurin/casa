de14351f7b829d544b2d5df4999bb7a01408a523
Marcelo Maurin Martins <marcelomaurinmartins@gmail.com>
2026-09-14 05:36:30 -0300

    Versiona integracao Watch Android como 2.2.0

--- android/JarvisMobile/app/build.gradle.kts
+++ android/JarvisMobile/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "br.com.maurinsoft.jarvismobile"
         minSdk = 26
         targetSdk = 35
-        versionCode = 210
-        versionName = "2.1.0"
+        versionCode = 220
+        versionName = "2.2.0"
     }
 
     buildTypes {
