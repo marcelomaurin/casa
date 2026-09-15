d359223c784f30a61e8c891e8be1354509f5ed5e
Marcelo Maurin Martins <marcelomaurinmartins@gmail.com>
2026-09-15 16:19:19 -0300

    Documenta pinos BMA motor microfone e audio do T-Watch V3

--- arduino/lilygo_watch/jarvis_watch/config.h
+++ arduino/lilygo_watch/jarvis_watch/config.h
@@ -20,6 +20,12 @@
 #define JARVIS_PDM_DATA_PIN     2
 #define JARVIS_PDM_CLK_PIN      0
 #define JARVIS_MOTION_INT_PIN   39
+#define JARVIS_MOTOR_PIN        4
+
+// MAX98357A / I2S de saida do T-Watch 2020 V3.
+#define JARVIS_AUDIO_BCK_PIN    26
+#define JARVIS_AUDIO_WS_PIN     25
+#define JARVIS_AUDIO_DOUT_PIN   33
 
 // O Watch pode armazenar localmente apenas perfis Wi-Fi domésticos autorizados
 // e um token individual de dispositivo com escopos mínimos. Nunca armazenar
