0f65aad5a569c7cf021a85c47867f9a88b629189
Marcelo Maurin Martins <marcelomaurinmartins@gmail.com>
2026-09-15 17:10:48 -0300

    Atualiza JARVIS Mobile para 2.4.1

--- android/JarvisMobile/app/build.gradle.kts
+++ android/JarvisMobile/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "br.com.maurinsoft.jarvismobile"
         minSdk = 26
         targetSdk = 35
-        versionCode = 240
-        versionName = "2.4.0"
+        versionCode = 241
+        versionName = "2.4.1"
     }
 
     buildTypes {
