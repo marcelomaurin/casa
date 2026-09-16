b61575865370071eb50bf32e59792905cf42920a
Marcelo Maurin Martins <marcelomaurinmartins@gmail.com>
2026-09-16 14:27:02 -0300

    Atualiza JARVIS Mobile para 2.6.2 sem bloqueio por sessao online

--- android/JarvisMobile/app/build.gradle.kts
+++ android/JarvisMobile/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "br.com.maurinsoft.jarvismobile"
         minSdk = 26
         targetSdk = 35
-        versionCode = 261
-        versionName = "2.6.1"
+        versionCode = 262
+        versionName = "2.6.2"
     }
 
     buildTypes {
