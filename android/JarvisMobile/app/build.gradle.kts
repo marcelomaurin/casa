8836efe41fa3b53c661cfd66b63fcde0fb758790
Marcelo Maurin Martins <marcelomaurinmartins@gmail.com>
2026-09-16 14:08:07 -0300

    Atualiza JARVIS Mobile para 2.6.0 com login e menu LCARS

--- android/JarvisMobile/app/build.gradle.kts
+++ android/JarvisMobile/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "br.com.maurinsoft.jarvismobile"
         minSdk = 26
         targetSdk = 35
-        versionCode = 250
-        versionName = "2.5.0"
+        versionCode = 260
+        versionName = "2.6.0"
     }
 
     buildTypes {
