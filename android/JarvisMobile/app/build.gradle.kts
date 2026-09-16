5c855443ab8cd7b96fd3e54b7b8fd1d1fa634ba4
Marcelo Maurin Martins <marcelomaurinmartins@gmail.com>
2026-09-16 14:18:35 -0300

    Atualiza JARVIS Mobile para 2.6.1 com permissoes sob demanda

--- android/JarvisMobile/app/build.gradle.kts
+++ android/JarvisMobile/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "br.com.maurinsoft.jarvismobile"
         minSdk = 26
         targetSdk = 35
-        versionCode = 260
-        versionName = "2.6.0"
+        versionCode = 261
+        versionName = "2.6.1"
     }
 
     buildTypes {
