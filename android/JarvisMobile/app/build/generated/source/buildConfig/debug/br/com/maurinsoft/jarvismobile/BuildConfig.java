17ec8b39aaf98b4d205786be08a62d5f7ca0e5c1
Marcelo Maurin <Mmaurin@saude.lan>
2026-09-16 08:28:57 -0300

    correcoes

---
+++ android/JarvisMobile/app/build/generated/source/buildConfig/debug/br/com/maurinsoft/jarvismobile/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package br.com.maurinsoft.jarvismobile;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "br.com.maurinsoft.jarvismobile.debug";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 241;
+  public static final String VERSION_NAME = "2.4.1-debug";
+}
