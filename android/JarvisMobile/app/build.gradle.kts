363d7993de21eeb4df63d888676d4771736c282b
Marcelo Maurin Martins <marcelomaurinmartins@gmail.com>
2026-09-16 13:55:12 -0300

    Atualiza Jarvis Mobile para 2.4.3 com socket local do Watch

--- android/JarvisMobile/app/build.gradle.kts
+++ android/JarvisMobile/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "br.com.maurinsoft.jarvismobile"
         minSdk = 26
         targetSdk = 35
-        versionCode = 242
-        versionName = "2.4.2"
+        versionCode = 243
+        versionName = "2.4.3"
     }
 
     buildTypes {
