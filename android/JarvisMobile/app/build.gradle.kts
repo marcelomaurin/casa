1ffb2b719c52a704461399cc815a44f78839a7c4
Marcelo Maurin Martins <marcelomaurinmartins@gmail.com>
2026-09-16 14:04:13 -0300

    Atualiza JARVIS Mobile para 2.5.0 com nova navegacao

--- android/JarvisMobile/app/build.gradle.kts
+++ android/JarvisMobile/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "br.com.maurinsoft.jarvismobile"
         minSdk = 26
         targetSdk = 35
-        versionCode = 243
-        versionName = "2.4.3"
+        versionCode = 250
+        versionName = "2.5.0"
     }
 
     buildTypes {
