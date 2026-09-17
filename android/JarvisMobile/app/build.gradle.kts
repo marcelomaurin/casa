51c4fa3787be370d587710a70831e4b1589f6f2d
Marcelo Maurin Martins <marcelomaurinmartins@gmail.com>
2026-09-17 16:08:54 -0300

    Versao 2.6.6 com atualizacao automatica

--- android/JarvisMobile/app/build.gradle.kts
+++ android/JarvisMobile/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "br.com.maurinsoft.jarvismobile"
         minSdk = 26
         targetSdk = 35
-        versionCode = 265
-        versionName = "2.6.5"
+        versionCode = 266
+        versionName = "2.6.6"
     }
 
     buildTypes {
