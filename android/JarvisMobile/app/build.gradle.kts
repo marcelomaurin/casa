8777b0450e4c621e711db1d41d7d95e1a703acc4
Marcelo Maurin Martins <marcelomaurinmartins@gmail.com>
2026-09-17 15:17:01 -0300

    Versao 2.6.5 com cenas no dashboard

--- android/JarvisMobile/app/build.gradle.kts
+++ android/JarvisMobile/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "br.com.maurinsoft.jarvismobile"
         minSdk = 26
         targetSdk = 35
-        versionCode = 264
-        versionName = "2.6.4"
+        versionCode = 265
+        versionName = "2.6.5"
     }
 
     buildTypes {
