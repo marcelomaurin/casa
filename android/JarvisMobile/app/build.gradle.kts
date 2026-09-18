a3d57c0a38d6ecc86af1e6ef4605693f70bec61a
Marcelo Maurin Martins <marcelomaurinmartins@gmail.com>
2026-09-18 16:49:39 -0300

    chore(android): bump mobile version to 2.6.7

--- android/JarvisMobile/app/build.gradle.kts
+++ android/JarvisMobile/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "br.com.maurinsoft.jarvismobile"
         minSdk = 26
         targetSdk = 35
-        versionCode = 266
-        versionName = "2.6.6"
+        versionCode = 267
+        versionName = "2.6.7"
     }
 
     buildTypes {
