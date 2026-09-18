f977b3b9ec30ffcb6254d899b5fe4083d205129a
Marcelo Maurin Martins <marcelomaurinmartins@gmail.com>
2026-09-18 11:33:34 -0300

    Move controles administrativos para a coluna de status

--- site/var/www/html/index.php
+++ site/var/www/html/index.php
@@ -9,17 +9,17 @@ $authName = !empty($_SESSION['auth_name']) ? $_SESSION['auth_name'] : ($_SESSION
   <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
   <title>CASA / COMPUTER</title>
   <link rel="stylesheet" href="/casa/lcars-framework.css?v=1.2.0">
-  <link rel="stylesheet" href="/casa/lcars-site.css?v=1.4.0">
+  <link rel="stylesheet" href="/casa/lcars-site.css?v=1.4.2">
 </head>
 <body>
-  <div class="ja-site-tools" aria-label="Ferramentas do sistema">
-    <button class="home" type="button" onclick="CASASite.home()">GRUPOS</button>
-    <button type="button" onclick="CASASite.changePassword()">ALTERAR SENHA</button>
-    <a href="/casa/login.php?logout=1">SAIR · <?= htmlspecialchars($authName, ENT_QUOTES, 'UTF-8') ?></a>
+  <div class="ja-admin-template" id="ja-admin-template" aria-hidden="true">
+    <button class="home" type="button">GRUPOS</button>
+    <button class="password" type="button">ALTERAR SENHA</button>
+    <a class="logout" href="/casa/login.php?logout=1">SAIR · <?= htmlspecialchars($authName, ENT_QUOTES, 'UTF-8') ?></a>
   </div>
   <div id="app"></div>
   <noscript>O CASA/COMPUTER precisa de JavaScript habilitado para a interface adaptativa.</noscript>
   <script src="/casa/lcars-framework.js?v=1.2.0"></script>
-  <script src="/casa/lcars-site.js?v=1.4.0"></script>
+  <script src="/casa/lcars-site.js?v=1.4.2"></script>
 </body>
 </html>
