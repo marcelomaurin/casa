2c4a1feb96075e89edd9df9b336d42d975f9162e
Marcelo Maurin Martins <marcelomaurinmartins@gmail.com>
2026-09-18 10:29:16 -0300

    Atualiza cache dos assets LCARS

--- site/var/www/html/index.php
+++ site/var/www/html/index.php
@@ -8,8 +8,8 @@ $authName = !empty($_SESSION['auth_name']) ? $_SESSION['auth_name'] : ($_SESSION
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
   <title>CASA / JARVIS</title>
-  <link rel="stylesheet" href="/casa/lcars-framework.css?v=1.1.0">
-  <link rel="stylesheet" href="/casa/lcars-site.css?v=1.1.0">
+  <link rel="stylesheet" href="/casa/lcars-framework.css?v=1.2.0">
+  <link rel="stylesheet" href="/casa/lcars-site.css?v=1.2.0">
 </head>
 <body>
   <div class="ja-site-tools" aria-label="Ferramentas do sistema">
@@ -18,7 +18,7 @@ $authName = !empty($_SESSION['auth_name']) ? $_SESSION['auth_name'] : ($_SESSION
   </div>
   <div id="app"></div>
   <noscript>O CASA/JARVIS precisa de JavaScript habilitado para a interface adaptativa.</noscript>
-  <script src="/casa/lcars-framework.js?v=1.1.0"></script>
-  <script src="/casa/lcars-site.js?v=1.1.0"></script>
+  <script src="/casa/lcars-framework.js?v=1.2.0"></script>
+  <script src="/casa/lcars-site.js?v=1.2.0"></script>
 </body>
 </html>
