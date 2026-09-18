69d7fc3ad2e3c925b349c06d649aa6f7cfef9994
Marcelo Maurin Martins <marcelomaurinmartins@gmail.com>
2026-09-18 11:38:23 -0300

    Atualiza cache do layout de telemetria

--- site/var/www/html/index.php
+++ site/var/www/html/index.php
@@ -9,7 +9,7 @@ $authName = !empty($_SESSION['auth_name']) ? $_SESSION['auth_name'] : ($_SESSION
   <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
   <title>CASA / COMPUTER</title>
   <link rel="stylesheet" href="/casa/lcars-framework.css?v=1.2.0">
-  <link rel="stylesheet" href="/casa/lcars-site.css?v=1.4.2">
+  <link rel="stylesheet" href="/casa/lcars-site.css?v=1.4.3">
 </head>
 <body>
   <div class="ja-admin-template" id="ja-admin-template" aria-hidden="true">
@@ -19,7 +19,7 @@ $authName = !empty($_SESSION['auth_name']) ? $_SESSION['auth_name'] : ($_SESSION
   </div>
   <div id="app"></div>
   <noscript>O CASA/COMPUTER precisa de JavaScript habilitado para a interface adaptativa.</noscript>
-  <script src="/casa/lcars-framework.js?v=1.2.0"></script>
-  <script src="/casa/lcars-site.js?v=1.4.2"></script>
+  <script src="/casa/lcars-framework.js?v=1.3.0"></script>
+  <script src="/casa/lcars-site.js?v=1.4.3"></script>
 </body>
 </html>
