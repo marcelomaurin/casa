c33b71aecc58a9198991a8ec95172efb48ab77b5
Marcelo Maurin Martins <marcelomaurinmartins@gmail.com>
2026-09-18 10:36:21 -0300

    Atualiza identidade COMPUTER e cache do módulo de voz

--- site/var/www/html/index.php
+++ site/var/www/html/index.php
@@ -7,7 +7,7 @@ $authName = !empty($_SESSION['auth_name']) ? $_SESSION['auth_name'] : ($_SESSION
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
-  <title>CASA / JARVIS</title>
+  <title>CASA / COMPUTER</title>
   <link rel="stylesheet" href="/casa/lcars-framework.css?v=1.2.0">
   <link rel="stylesheet" href="/casa/lcars-site.css?v=1.2.1">
 </head>
@@ -18,8 +18,8 @@ $authName = !empty($_SESSION['auth_name']) ? $_SESSION['auth_name'] : ($_SESSION
     <a href="/casa/login.php?logout=1">SAIR · <?= htmlspecialchars($authName, ENT_QUOTES, 'UTF-8') ?></a>
   </div>
   <div id="app"></div>
-  <noscript>O CASA/JARVIS precisa de JavaScript habilitado para a interface adaptativa.</noscript>
+  <noscript>O CASA/COMPUTER precisa de JavaScript habilitado para a interface adaptativa.</noscript>
   <script src="/casa/lcars-framework.js?v=1.2.0"></script>
-  <script src="/casa/lcars-site.js?v=1.2.1"></script>
+  <script src="/casa/lcars-site.js?v=1.2.2"></script>
 </body>
 </html>
