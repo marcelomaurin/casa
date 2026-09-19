25d3edf04f657a577b95d435037886cf22ca9fab
Marcelo Maurin Martins <marcelomaurinmartins@gmail.com>
2026-09-19 13:40:21 -0300

    fix(android): restore permission package manager import

--- android/JarvisMobile/app/src/main/java/br/com/maurinsoft/jarvismobile/MainActivity.kt
+++ android/JarvisMobile/app/src/main/java/br/com/maurinsoft/jarvismobile/MainActivity.kt
@@ -2,6 +2,7 @@ package br.com.maurinsoft.jarvismobile
 
 import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
