f36a3e66745468f765e2a66ed4e1966b0f35c285
Marcelo Maurin Martins <marcelomaurinmartins@gmail.com>
2026-09-19 13:36:45 -0300

    refactor(android): extract MobileRoute

---
+++ android/JarvisMobile/app/src/main/java/br/com/maurinsoft/jarvismobile/MobileRoute.kt
@@ -0,0 +1,6 @@
+package br.com.maurinsoft.jarvismobile
+
+enum class MobileRoute {
+    HOME, CASA_MENU, JARVIS_MENU, WATCH_MENU, DEVICES_MENU, SYSTEM_MENU,
+    OPERATIONS, VOICE, WATCH_STATUS, DEVICES_LIST, CONFIG
+}
